d9b05683bb7d40f3660792435eea595d3fc48ad8
Lucas Quaroni <112420267+LucasQuaroni@users.noreply.github.com>
2025-10-14 19:41:05 -0300

    feat: Integrate React Router for navigation and remove App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
+import Layout from './Layout.tsx'
+import About from './About.tsx'
+import Contact from './Contact.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: App,
+        errorElement: <div>Error</div>,
+      },
+      {
+        path: "about",
+        Component: About,
+        errorElement: <div>Error</div>,
+      },
+      {
+        path: "contact",
+        Component: Contact,
+        errorElement: <div>Error</div>,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
